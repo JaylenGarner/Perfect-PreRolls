5e643c29f84db5d7a526a3c0bae64944b4e0dca5
Jaylen Garner <jaylen@moonraydevelopment.com>
2023-12-06 21:29:22 -0500

    Testing re-processed video

--- components/MachineAnimation.jsx
+++ components/MachineAnimation.jsx
@@ -12,7 +12,7 @@ const MachineAnimation = () => {
     >
       <video autoPlay loop muted playsInline className="machine_animation">
         <source
-          src="/loader-animation.mp4"
+          src="https://perfect-prerolls.s3.amazonaws.com/Public/Assets/animation-test.mp4"
           type="video/mp4"
           className="max-md:hidden"
         />
