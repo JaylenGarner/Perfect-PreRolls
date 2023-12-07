91e313d4fd0218f2aa6bd2e42fabb35c46aa0667
Jaylen Garner <jaylen@moonraydevelopment.com>
2023-12-06 20:14:56 -0500

    Testing animation as WebM

--- components/MachineAnimation.jsx
+++ components/MachineAnimation.jsx
@@ -10,16 +10,9 @@ const MachineAnimation = () => {
       animate={{ opacity: 1 }}
       transition={{ duration: 1 }}
     >
-      <video
-        autoPlay
-        controls
-        loop
-        muted
-        playsInline
-        className="machine_animation"
-      >
+      <video autoPlay loop muted playsInline className="machine_animation">
         <source
-          src="https://perfect-prerolls.s3.amazonaws.com/Public/Assets/loader-360-animation.mp4"
+          src="https://perfect-prerolls.s3.amazonaws.com/Public/Assets/animation-test.mp4"
           type="video/mp4"
           className="max-md:hidden"
         />
